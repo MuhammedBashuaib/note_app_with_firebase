061c785874bbc7a83714bc346283487a2b7ce07b
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-19 12:33:00 +0300

    CREATE signUp cubit and ADD states to signUp state

---
+++ lib/cubits/sign_up_cubit/sign_up_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit() : super(SignUpInitial());
+}
