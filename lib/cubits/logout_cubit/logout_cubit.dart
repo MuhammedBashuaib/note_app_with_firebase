0ade8bec5e944439ea8b0e303f1bf48cf4b744f7
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-20 16:17:26 +0300

    CREATE logout mehtod in logout cubit

--- lib/cubits/logout_cubit/logout_cubit.dart
+++ lib/cubits/logout_cubit/logout_cubit.dart
@@ -1,8 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app_with_firebase/data/services/auth_services/auth_service.dart';
 
 part 'logout_state.dart';
 
 class LogoutCubitCubit extends Cubit<LogoutCubitState> {
   LogoutCubitCubit() : super(LogoutCubitInitial());
+
+  final AuthService _authService = AuthService();
+
+  Future<void> logout() async {
+    emit(LogoutLoadingState());
+    try {
+      await _authService.logout();
+      emit(LogoutSuccessState());
+    } on Exception catch (e) {
+      emit(
+        LogoutFailureState(
+          erorrMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
