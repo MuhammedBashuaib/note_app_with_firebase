85d545d39961affc505734a26e5413c6ca887ec7
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-23 13:02:43 +0300

    create delete category method in delete category cubit

--- lib/cubits/delete_category_cubit/delete_category_cubit.dart
+++ lib/cubits/delete_category_cubit/delete_category_cubit.dart
@@ -1,8 +1,25 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app_with_firebase/data/services/firestore_services/category_service.dart';
 
 part 'delete_category_state.dart';
 
 class DeleteCategoryCubit extends Cubit<DeleteCategoryState> {
   DeleteCategoryCubit() : super(DeleteCategoryInitial());
+
+  final CategoryService _categoryService = CategoryService();
+
+  Future<void> deleteCategory({required String categoryId}) async {
+    emit(DeleteCategoryLoadingState());
+    try {
+      _categoryService.deleteCategory(id: categoryId);
+      emit(DeleteCategorySuccessState());
+    } on Exception catch (e) {
+      emit(
+        DeleteCategoryFailureState(
+          erorrMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
