0afb69508a5c9a209bb07bf8f75e6cb191ccb448
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-23 13:11:10 +0300

    provide delete category cubit in home screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app_with_firebase/cubits/delete_category_cubit/delete_category_cubit.dart';
 
 import 'package:note_app_with_firebase/cubits/logout_cubit/logout_cubit.dart';
 import 'package:note_app_with_firebase/presentation/widgets/custom_logout_button.dart';
@@ -13,8 +14,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LogoutCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LogoutCubit>(
+          create: (context) => LogoutCubit(),
+        ),
+        BlocProvider<DeleteCategoryCubit>(
+          create: (context) => DeleteCategoryCubit(),
+        ),
+      ],
       child: BlocConsumer<LogoutCubit, LogoutState>(
         listener: (context, state) {
           if (state is LogoutSuccessState) {
