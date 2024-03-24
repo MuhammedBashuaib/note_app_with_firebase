76940da75bf647e61e90720273c33488487ed488
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-25 00:07:58 +0300

    ADD states to edit category state

--- lib/cubits/edit_category_cubit/edit_category_state.dart
+++ lib/cubits/edit_category_cubit/edit_category_state.dart
@@ -4,3 +4,15 @@ part of 'edit_category_cubit.dart';
 sealed class EditCategoryState {}
 
 final class EditCategoryInitial extends EditCategoryState {}
+
+final class EditCategoryLoadingState extends EditCategoryState {}
+
+final class EditCategorySuccessState extends EditCategoryState {}
+
+final class EditCategoryFailureState extends EditCategoryState {
+  final String erorrMessage;
+
+  EditCategoryFailureState({
+    required this.erorrMessage,
+  });
+}
