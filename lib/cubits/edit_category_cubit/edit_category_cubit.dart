22052dbfb1c302198118f8bb7bee3eb93d001a42
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-25 01:33:34 +0300

    ADD edit category method to edit category cubit

--- lib/cubits/edit_category_cubit/edit_category_cubit.dart
+++ lib/cubits/edit_category_cubit/edit_category_cubit.dart
@@ -1,8 +1,31 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app_with_firebase/data/services/firestore_services/category_service.dart';
 
 part 'edit_category_state.dart';
 
 class EditCategoryCubit extends Cubit<EditCategoryState> {
   EditCategoryCubit() : super(EditCategoryInitial());
+
+  final CategoryService _categoryService = CategoryService();
+
+  Future<void> editCategory({
+    required String categoryId,
+    required String categorName,
+  }) async {
+    emit(EditCategoryLoadingState());
+    try {
+      await _categoryService.editCategory(
+        categoryId: categoryId,
+        newName: categorName,
+      );
+      emit(EditCategorySuccessState());
+    } on Exception catch (e) {
+      emit(
+        EditCategoryFailureState(
+          erorrMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
