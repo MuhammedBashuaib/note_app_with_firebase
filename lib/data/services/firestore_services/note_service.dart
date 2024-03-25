ad9d60f0864e205248cb48ae5cdff6ae8c326ad6
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-25 15:32:41 +0300

    ADD get all notes  method to note service

--- lib/data/services/firestore_services/note_service.dart
+++ lib/data/services/firestore_services/note_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:note_app_with_firebase/data/models/note_model.dart';
 import 'package:note_app_with_firebase/res/const.dart';
 
 class NoteService {
@@ -10,10 +11,10 @@ class NoteService {
     required String noteTitle,
     required note,
   }) async {
-    CollectionReference notes =
+    CollectionReference notesCollection =
         _categories.doc(categoryId).collection(MyCollictions.kNoteCollictions);
 
-    return notes
+    return notesCollection
         .add(
           {
             MyNotekeys.kNoteTitle: noteTitle,
@@ -27,4 +28,39 @@ class NoteService {
           (error) => error.toString(),
         );
   }
+
+  Future<List<NoteModel>> getAllNotes({
+    required String categoryId,
+  }) async {
+    List<QueryDocumentSnapshot> data = [];
+    List<NoteModel> notes = [];
+    CollectionReference notesCollection =
+        _categories.doc(categoryId).collection(MyCollictions.kNoteCollictions);
+    QuerySnapshot querySnapshot = await notesCollection.get();
+    data.addAll(querySnapshot.docs);
+
+    for (var element in data) {
+      notes.add(
+        NoteModel.fromFirestore(element, categoryId),
+      );
+    }
+
+    return notes;
+  }
+
+  // Future<List<CategoryModel>> getAllCategories({
+  //   required String uid,
+  // }) async {
+  //   List<QueryDocumentSnapshot> data = [];
+  //   List<CategoryModel> categories = [];
+  //   QuerySnapshot querySnapshot =
+  //       await _categories.where("uid", isEqualTo: uid).get();
+  //   data.addAll(querySnapshot.docs);
+
+  //   for (var element in data) {
+  //     categories.add(CategoryModel.fromFirestore(element));
+  //   }
+
+  //   return categories;
+  // }
 }
