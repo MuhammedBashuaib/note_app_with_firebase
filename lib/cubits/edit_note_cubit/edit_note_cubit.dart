482272d315f87e54596f41b57555131393c46eaa
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-27 01:23:08 +0300

    create edit note method in edit note cubit

--- lib/cubits/edit_note_cubit/edit_note_cubit.dart
+++ lib/cubits/edit_note_cubit/edit_note_cubit.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app_with_firebase/data/services/firestore_services/note_service.dart';
 
 part 'edit_note_state.dart';
 
 class EditNoteCubit extends Cubit<EditNoteState> {
   EditNoteCubit() : super(EditNoteInitial());
+
+  final NoteService _noteService = NoteService();
+
+  Future<void> editNote({
+    required String categoryId,
+    required String noteId,
+    required String newNoteTitle,
+    required String newNote,
+  }) async {
+    emit(EditNoteLoadingState());
+    try {
+      await _noteService.editNote(
+        categoryId: categoryId,
+        noteId: noteId,
+        newNoteTitle: newNoteTitle,
+        newNote: newNote,
+      );
+      emit(EditNoteSuccessState());
+    } on Exception catch (e) {
+      emit(
+        EditNoteFailureState(
+          erorrMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
