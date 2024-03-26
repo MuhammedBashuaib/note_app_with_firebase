acd523e041488c6b0ae73517eae9b62ce60d9dbc
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-27 00:21:58 +0300

    create delete note method in delete note cubit

--- lib/cubits/delete_note_cubit/delete_note_cubit.dart
+++ lib/cubits/delete_note_cubit/delete_note_cubit.dart
@@ -1,8 +1,31 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:note_app_with_firebase/data/services/firestore_services/note_service.dart';
 
 part 'delete_note_state.dart';
 
 class DeleteNoteCubit extends Cubit<DeleteNoteState> {
   DeleteNoteCubit() : super(DeleteNoteInitial());
+
+  final NoteService _noteService = NoteService();
+
+  Future<void> deleteNote(
+      {required String categoryId, required String noteId}) async {
+    emit(DeleteNoteLoadingState());
+    try {
+      _noteService.deleteNote(
+        categoryId: categoryId,
+        id: noteId,
+      );
+      emit(
+        DeleteNoteSuccessState(),
+      );
+    } on Exception catch (e) {
+      emit(
+        DeleteNoteFailureState(
+          erorrMessage: e.toString(),
+        ),
+      );
+    }
+  }
 }
