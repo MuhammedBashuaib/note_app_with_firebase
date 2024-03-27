67b1882d4a15f26bf71ac080f8c969ff5986f320
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-03-28 01:46:49 +0300

    ADD image picker package to pubspec.yaml and create image picker file

---
+++ lib/helper/image_picker.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+final picker = ImagePicker();
+File? imageFile;
+Future<File> pickImageFormGallery() async {
+  final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+  if (pickedFile != null) {
+    imageFile = File(pickedFile.path);
+  }
+  return imageFile!;
+}
+
+Future<File> pickImageFormCamera() async {
+  File? imageFile;
+  final pickedFile = await picker.pickImage(source: ImageSource.camera);
+  if (pickedFile != null) {
+    imageFile = File(pickedFile.path);
+  }
+  return imageFile!;
+}
