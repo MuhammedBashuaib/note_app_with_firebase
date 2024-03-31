2c89b942e843a946c044b5853f291072dc031299
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-04-01 01:34:33 +0300

    UPD note model

--- lib/data/models/note_model.dart
+++ lib/data/models/note_model.dart
@@ -4,22 +4,26 @@ import 'package:note_app_with_firebase/res/const.dart';
 class NoteModel {
   String id;
   String categoryId;
+  String imageUrl;
   String noteTitle;
   String note;
   DateTime createdDate;
 
-  NoteModel(
-      {required this.id,
-      required this.categoryId,
-      required this.noteTitle,
-      required this.note,
-      required this.createdDate});
+  NoteModel({
+    required this.id,
+    required this.imageUrl,
+    required this.categoryId,
+    required this.noteTitle,
+    required this.note,
+    required this.createdDate,
+  });
 
   factory NoteModel.fromFirestore(DocumentSnapshot doc, categoryId) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return NoteModel(
       id: doc.id,
       categoryId: categoryId,
+      imageUrl: data[MyNotekeys.kImageUrl],
       noteTitle: data[MyNotekeys.kNoteTitle],
       note: data[MyNotekeys.kNote],
       createdDate: data[MyNotekeys.kCreatedDate].toDate(),
@@ -29,6 +33,7 @@ class NoteModel {
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       MyNotekeys.kId: id,
+      MyNotekeys.kImageUrl: imageUrl,
       MyNotekeys.kNoteTitle: noteTitle,
       MyNotekeys.kNote: note,
       MyNotekeys.kCreatedDate: createdDate,
